575a2d9009529509b5d85b8497e274fc914bb44e
Pushkar N <131258869+Pnarayan-3@users.noreply.github.com>
2026-09-21 16:53:50 +0530

    added about pod/deployment

--- kube-namespace.sh
+++ kube-namespace.sh
@@ -13,3 +13,9 @@ kubectl delete pod nginx -n nginx # to delete the pod inside nginx namespace
 kubectl delete ns nginx # to delete the namespace nginx
 kubectl apply -f namespace.yml # this used to create or update namespaces
 kubectl get ns # to display all namespce created or default
+
+# some pod commands
+kubectl apply -f pod.yml
+kubectl exec -it pod/nginx-pod . -n nginx --bash
+kubectl ddescribe pod/nginx-pod -n nginx # it will give every status 
+kubectl scale deployment/nginx-deployment -n nginx --replicas=5 # scale the replica to 5 from 2 having the deployment named nginx-deployment
