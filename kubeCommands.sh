1f32edc6b0b94079a3171895b501b12fbdb996df
Pushkar N <131258869+Pnarayan-3@users.noreply.github.com>
2026-09-23 17:41:33 +0530

    added pv,pvc,svc

--- kubeCommands.sh
+++ kubeCommands.sh
@@ -1,8 +1,9 @@
 # some pod commands
 kubectl apply -f pod.yml
 kubectl exec -it pod/nginx-pod . -n nginx --bash
-kubectl ddescribe pod/nginx-pod -n nginx # it will give every status 
+kubectl describe pod/nginx-pod -n nginx # it will give every status 
 kubectl scale deployment/nginx-deployment -n nginx --replicas=5 # scale the replica to 5 from 2 having the deployment named nginx-deployment
+kubectl get-all -n nginx
 
 # to create replicaset and view them
 kubectl apply -f replicasets.yml
@@ -14,3 +15,11 @@ kubectl get cronjob -n nginx
 kubectl get pods -n nginx
 kubectl logs pod -n nginx
 kubectl delete -f cronjob.yml
+
+# some commands related with persistent volumes and persistent volume claims
+kubectl get pv
+kubectl get pvc
+
+# port forwarding
+kubectl port-forward service/nginx-service -n nginx 80:80 --address=0.0.0.0
+
